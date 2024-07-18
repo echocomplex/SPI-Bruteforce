76fd66ea4e42f7002d326f90b67ce090faca6eb0
echo complex <lexa33232@gmail.com>
2024-07-18 12:39:23 +0300

    Update

---
+++ SPIBruteforce.py
@@ -0,0 +1,20 @@
+import subprocess
+import random
+
+class SPIBruteforce:
+    def __init__ (self) -> None:
+        self.__alphabet: str = "ABCDEF012346789";
+
+    def start (self, lenght: int, USBPort: str) -> str:
+        while (True):
+            key: str = "".join(random.choices(self.__alphabet, k=lenght));
+            result = subprocess.run(["spiunlock.exe", "unlock", "key.txt", "-usb", USBPort], capture_output=True, text=True);
+            if ("ERROR" in result.stdout):
+                print("KEY ----> %s IS WRONG" % (key,));
+            else:
+                print("KEY ----> %s UNLOCKING THE CHIP" % (key,));
+                return key;
+
+if (__name__ == "__main__"):
+    unit = SPIBruteforce();
+    print("\n\n" + unit.start(32, "5435342"));
